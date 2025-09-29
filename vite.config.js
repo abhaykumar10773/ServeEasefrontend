cab3f5013348a446dbea6abcfbeb678a51605da5
abhaykumar10773 <abhay885355@gmail.com>
2025-09-29 21:22:20 +0530

    fix it

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -7,9 +7,22 @@ export default defineConfig({
   
   server: {
     proxy :{
-     "/api" : 'http:localhost:3000',
+     "/api" : 'http://localhost:3000',
     },
    },
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    chunkSizeWarningLimit: 1200,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['axios', 'socket.io-client']
+        }
+      }
+    }
+  },
   
 })
+
